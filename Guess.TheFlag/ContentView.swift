ac41c92b589dbf83751d5994261c07bef43a9453
JoshuaRosado <joshua06rosado@gmail.com>
2024-09-03 07:15:39 -0500

    Initial Commit

---
+++ Guess.TheFlag/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Guess.TheFlag
+//
+//  Created by Joshua Rosado Olivencia on 9/3/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Guess.TheFlag/Guess_TheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  Guess_TheFlagApp.swift
+//  Guess.TheFlag
+//
+//  Created by Joshua Rosado Olivencia on 9/3/24.
+//
+
+import SwiftUI
+
+@main
+struct Guess_TheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
